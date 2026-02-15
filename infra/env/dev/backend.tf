5ddf505dfc2e36dddddd4f64d7eeef84410b09a9
Aditya Agnihotri <agnihotria700@gmail.com>
2026-02-15 17:13:58 +0530

    added

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -4,6 +4,7 @@ terraform {
     key            = "env/dev/terraform.tfstate"
     region         = "ap-south-1" 
     dynamodb_table = "terraform-locks-dev"
-    state_lock = false
+    state_lock_table = "terraform-locks-dev"
+    encrypt        = false
   }
 }
